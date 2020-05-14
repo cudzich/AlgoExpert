bd49e087f2b0b11161390e5504a5b961355ac4d5
unknown <cudzich_m@outlook.com>
2020-05-14 13:21:15 -0500

    Solved subseqence validation

---
+++ Arrays/Validate Subsequence/isValidSubsequence.js
@@ -0,0 +1,17 @@
+// O(n) time | O(1) space
+function isValidSubsequence(array, sequence) {
+  let length = sequence.length;
+	let count = 0;
+	let sub_element = sequence[count];
+
+	for (var i = 0; i < array.length; i++) {
+		if(array[i] == sub_element) {
+			count++;
+			if( count == length) {
+				return true
+			}
+			sub_element = sequence[count];
+		}
+	}
+	return false;
+}
