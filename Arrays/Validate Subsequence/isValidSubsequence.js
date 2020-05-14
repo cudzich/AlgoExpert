a7e243b6af64785e4ffedfd5f46c1c648a3b7060
cudzich <cudzich_m@outlook.com>
2020-05-14 13:25:43 -0500

    formatting

--- Arrays/Validate Subsequence/isValidSubsequence.js
+++ Arrays/Validate Subsequence/isValidSubsequence.js
@@ -1,17 +1,18 @@
 // O(n) time | O(1) space
 function isValidSubsequence(array, sequence) {
   let length = sequence.length;
-	let count = 0;
-	let sub_element = sequence[count];
+  let count = 0;
+  let sub_element = sequence[count];
 
-	for (var i = 0; i < array.length; i++) {
-		if(array[i] == sub_element) {
-			count++;
-			if( count == length) {
-				return true
-			}
-			sub_element = sequence[count];
-		}
+   for (var i = 0; i < array.length; i++) {
+     if(array[i] == sub_element) {
+       count++;
+       if( count == length) {
+	  return true
 	}
-	return false;
+      sub_element = sequence[count];
+     }
+   }
+	
+  return false;
 }
